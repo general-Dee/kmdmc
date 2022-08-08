9c717c7eac3e69abf111445f5aaf5397c5aeed9b
general-Dee <oshinowodare@gmail.com>
2022-08-08 13:51:00 +0100

    standard04

--- components/NotFoundFooter.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NotFoundFooter() {
-  return (
-    <div className='fixed bottom-0 w-full p-20 bg-slate-600 text-white flex justify-center'>Copywrite@ KMDMC</div>
-  )
-}
-
-export default NotFoundFooter
