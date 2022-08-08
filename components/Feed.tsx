4d6875d3a8c79810de95f98ea335571e4bb7e784
general-Dee <oshinowodare@gmail.com>
2022-08-08 13:41:11 +0100

    standard03

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,12 +1,12 @@
 import { Box } from '@mui/material'
 import React from 'react'
-import Post from './Post'
+// import Post from './Post'
 
 function Feed() {
   return (
     // <Box bgcolor={"pink"} flex={2} p={2}>Posts</Box>
     <div className=''>
-      <Post title ="I am a post" body = "Lorem ipsim" />
+      {/* <Post title ="I am a post" body = "Lorem ipsim" /> */}
     </div>
     )
 }
