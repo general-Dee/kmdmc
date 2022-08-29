35a5f5a83765795d3f56c8d89d1b2ed34d687526
general-Dee <oshinowodare@gmail.com>
2022-08-29 15:07:07 +0100

    endtime12

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -35,7 +35,7 @@ function projects() {
               <Link href={"/projects/galaxymall"}>
                 <div className="lg:w-1/3 sm:w-1/2 p-4 cursor-pointer">
                     <div className="flex relative">
-                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/img/galaxymall/1.jpg"/>
+                    <img alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="/img/galaxymall/6.jpg"/>
                     <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                         <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">KADUNA NORTH LGA</h2>
                         <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Galaxy Mall</h1>
