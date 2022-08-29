b1cfb042160c5ec8b9b1346930d216e35ec4caf5
general-Dee <oshinowodare@gmail.com>
2022-08-29 11:57:00 +0100

    endtime10

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -65,7 +65,7 @@ function gallery() {
       <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto flex flex-wrap">
     <div className="flex w-full mb-20 flex-wrap">
-      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">TRADEFAIR </h1>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">TRADE FAIR </h1>
       <p className="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">KMDMC organized a Trade Fair on 1st and 2nd July 2022 at Murtala Muhammed Square in line with our commitment to provide safe and conducive environment for trading activities to thrive in Kaduna state.</p>
     </div>
     <div className="flex flex-wrap md:-m-2 -m-1">
