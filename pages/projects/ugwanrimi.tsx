aaf59a154595e4142862c491f140249f564c5793
general-Dee <oshinowodare@gmail.com>
2022-09-26 10:23:10 +0100

    detachment

--- pages/projects/ugwanrimi.tsx
+++ pages/projects/ugwanrimi.tsx
@@ -12,7 +12,7 @@ function ugwanrimi() {
       <section className="text-gray-600 body-font mt-20">
         <div className="container px-5 pt-24 mx-auto flex flex-wrap">
           <div className="flex w-full mb-20 flex-wrap">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Ungwanrimi Market</h1>
+            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4">Ungwan Rimi Market</h1>
             <p className="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Unguwan Rimi market is a well designed commercial hub that is in line with KDSG's urban renewal agenda. It contains 1340 shops of different sqm sizes, warehouses, restrooms and car parking space.</p>
           </div>
           <div className="flex flex-wrap md:-m-2 -m-1">
