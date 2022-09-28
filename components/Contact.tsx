ed0d478d3b7f06e93ff8959a1afc4b944c26b152
general-Dee <oshinowodare@gmail.com>
2022-09-28 22:42:50 +0100

    ignor

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -43,106 +43,68 @@ function Contact_sec() {
     }
     
   return (
-        <section className="text-gray-600 body-font relative">
-        <div className="container px-5 pt-24 pb-0 mx-auto flex sm:flex-nowrap flex-wrap">
-            <motion.div 
-            initial= {{
-                opacity: 0,
-                x: -100
-            }}
-            transition= {{duration: 1.2}}
-            whileInView= {{ opacity: 1, x: 0}}
-            viewport= {{ once: true}} 
-            className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            {/* <Image width="100%" height="100%" src="/map_image.PNG" className='absolute top-0 left-0 w-full h-full'/> */}
-                <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
-                    <div className="lg:w-1/2 px-6">
-                    <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-                    <p className="mt-1">Namabi Sambo Complex First Floor - Former Investment House, 27 Ali Akilu Road, Kaduna.</p>
-                    </div>
-                    <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                    <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-                    <a className="text-indigo-500 leading-relaxed">kaduna.markets@kdsg.gov.ng</a>
-                    <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
-                    <p className="leading-relaxed">+234 903 787 3878</p>
-                    </div>
-                </div>
-            </motion.div>
-                <motion.form 
-                 initial= {{
-                    opacity: 0,
-                    x: 100
-                }}
-                transition= {{duration: 1.4}}
-                whileInView= {{ opacity: 1, x: 0}}
-                viewport= {{ once: true}} 
-                id='contact_form' 
-                className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0" onSubmit={handleSubmit(onFormSubmit)}>
-                    <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">Contact</h2>
-                    <p className="leading-relaxed mb-5 text-gray-600">Leave us a comment below</p>
-                    <div className="relative mb-4">
-                        <label className="leading-7 text-sm text-gray-600">Name
-                            <input type="text" {...register("name", {required: true})} className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                        </label>
-                        {errors.name?.type === "required" && (
-                            <p>
-                                This field is required
-                            </p>
-                        )}
-                        {/* {errors.message?.type === "maxlength" && (
-                            <p>
-                                Maximum character length exceeded
-                            </p>
-                        )}
-                            {errors.message?.type === "pattern" && (
-                            <p>
-                                Letters only please
-                            </p>
-                        )} */}
-                    </div>
-                    <div className="relative mb-4">
-                        <label className="leading-7 text-sm text-gray-600">Email
-                            <input type="email" {...register("email", {required: true})} className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                            {errors.email?.type === "required" && (
-                            <p>
-                                This field is required
-                            </p>
-                        )}
-                        {/* {errors.message?.type === "maxlength" && (
-                            <p>
-                                Maximum character length exceeded
-                            </p>
-                        )} */}
-                        </label>
-                        
-                    </div>
-                    <div className="relative mb-4">
-                        <label className="leading-7 text-sm text-gray-600">Message
-                            <textarea {...register("message", {required: true})} className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                            {errors.message?.type === "required" && (
-                            <p>
-                                This field is required
-                            </p>
-                        )}
-                            {/* {errors.message?.type === "maxlength" && (
-                            <p>
-                                Maximum character length exceeded
-                            </p>
-                        )}
-                            {errors.message?.type === "pattern" && (
-                            <p>
-                                Letters only please
-                            </p>
-                        )} */}
-                        </label>
-                        
-                    </div>
-                    <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
-                    <p className="text-xs text-gray-500 mt-3">Contact KMDMC, we are sure to give you the answer you require and more</p>
-                </motion.form>
-                
+    <section className="text-gray-600 body-font relative">
+    <div className="container px-5 py-24 mx-auto">
+      <div className="flex flex-col text-center w-full mb-12">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+      </div>
+      <div className="lg:w-1/2 md:w-2/3 mx-auto">
+        <div className="flex flex-wrap -m-2">
+          <div className="p-2 w-1/2">
+            <div className="relative">
+              <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
+              <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+          </div>
+          <div className="p-2 w-1/2">
+            <div className="relative">
+              <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+              <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+            </div>
+          </div>
+          <div className="p-2 w-full">
+            <div className="relative">
+              <label for="message" className="leading-7 text-sm text-gray-600">Message</label>
+              <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+            </div>
+          </div>
+          <div className="p-2 w-full">
+            <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+          </div>
+          <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+            <a className="text-indigo-500">example@email.com</a>
+            <p className="leading-normal my-5">49 Smith St.
+              <br>Saint Cloud, MN 56301
+            </p>
+            <span className="inline-flex">
+              <a className="text-gray-500">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a className="ml-4 text-gray-500">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a className="ml-4 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+              <a className="ml-4 text-gray-500">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>
+              </a>
+            </span>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
   )
 }
 
