a2e418f9ee6d5cb4d7cf39ee1e5ccb56c1371539
general-Dee <oshinowodare@gmail.com>
2023-03-30 15:55:19 +0100

    video update

--- components/Video.tsx
+++ components/Video.tsx
@@ -7,8 +7,9 @@ function Video({}: Props) {
   return (
         <div className='aspect-w-16 aspect-h-9 mt-10 lg:mt-20'>
             <div className='w-full justify-items-center py-10'>
-                <iframe className='w-full h-[40vh] lg:h-[60vh]' src="https://www.youtube.com/embed/-sscEwLauyk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                <h1 className="text-xl font-medium text-center my-10 text-gray-900">End of Year Message (2019)</h1>
+                {/* <iframe className='w-full h-[40vh] lg:h-[60vh]' src="https://www.youtube.com/embed/-sscEwLauyk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
+                <iframe className='w-full h-[40vh] lg:h-[60vh]' width="560" height="315" src="https://www.youtube.com/embed/1AtiNkMq4Wg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                <h1 className="text-xl font-medium text-center my-10 text-gray-900">KMDMC PROJECTS</h1>
             </div>
         </div>
 
