3cc2a8ce9116a4ec3f5d48718cd725030a656326
general-Dee <oshinowodare@gmail.com>
2023-04-08 13:48:10 +0100

    Problems for head

--- components/About.tsx
+++ components/About.tsx
@@ -47,7 +47,7 @@ function About() {
                             <div className="flex flex-col items-center text-center justify-center">
                                 <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">Kaduna Markets Development and Management Company</h2>
                             <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                                <p className="text-base">We are currently managing over thirty(30) projects across the state, these projects cut across Commercial, Recreation and Hospitality.</p>
+                                <p className="text-base">We are currently managing over thirty(30) projects across the state, these projects cut across Commercial, Recreation and Hospitality. </p>
                             </div>
                         </div>
                         <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
