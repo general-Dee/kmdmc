b6e2b1d1e5b048b63edbafb1abca4426b0dc9697
general-Dee <oshinowodare@gmail.com>
2023-04-08 14:40:04 +0100

    Time dey go

--- pages/uploadImage.ts
+++
@@ -1,9 +0,0 @@
-import { storage } from '../firebase';
-
-export const uploadImage = async (file: File) => {
-  const storageRef = storage.ref();
-  const fileRef = storageRef.child(file.name);
-  await fileRef.put(file);
-  const imageUrl = await fileRef.getDownloadURL();
-  return imageUrl;
-};
